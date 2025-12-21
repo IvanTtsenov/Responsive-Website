2f076f6964e5ff8c5e4d37bedf0dda0b1b45897c
Ivan <ivan.cenov95@abv.bg>
2025-12-21 17:21:11 +0200

    added slot game for the second page, worked on html,css and js logic, created casino and index js files

---
+++ casino.js
@@ -0,0 +1,57 @@
+const casinoBtn = document.querySelector('.casino_start');
+const casinoPrizes = document.querySelectorAll('.game_contents');
+const casinoImages = document.querySelectorAll('.game_cards');
+const winner = document.querySelector('.winner');
+const winnerWrapepr = document.querySelector('.winner .wrapper');
+const winnerPop = document.querySelector('.pop-up');
+const closeBtn = document.querySelector('.close-btn');
+const winnerMessage = document.querySelector('.winningMessage');
+let randomIndex = null;
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+
+let isClicked = false;
+
+function gameLogic() {
+    casinoPrizes.forEach(prize => {
+        prize.classList.remove('random');
+    });
+
+    randomIndex = getRandomInt(casinoPrizes.length);
+    casinoPrizes[randomIndex].classList.add('random');
+}
+
+casinoBtn.addEventListener('click', () => {
+    if (isClicked) {
+        return;
+    }
+    isClicked = true;
+    casinoBtn.disabled = true;
+    casinoBtn.classList.add('disabled');
+    let intervalId = setInterval(gameLogic, 200);
+
+    setTimeout(() => {
+        clearInterval(intervalId);
+        isClicked = false;
+        casinoBtn.disabled = false;
+        casinoBtn.classList.remove('disabled');
+        prisePop(randomIndex);
+    }, 3000);
+});
+
+function prisePop(randomIndex) {
+    const prizeName = casinoImages[randomIndex].getAttribute('prize');
+    winnerPop.src = `../Sources/${prizeName}.png`
+    winner.classList.remove('hidden');
+    if(prizeName.toLowerCase() === "explode".toLowerCase()){
+        winnerMessage.textContent = "Oops! Try Again.";
+    }else {
+        winnerMessage.textContent = "Congratulations! You've won!"
+    }
+}
+
+closeBtn.addEventListener('click', () => {
+    winner.classList.add('hidden'); // hide the popup
+});
--- main.js
+++ main.js
@@ -4,10 +4,6 @@ const pagesLinks = document.querySelectorAll(".pages_links");
 const body = document.querySelector("body");
 const langBtn = document.querySelector(".language");
 const langMenu = document.querySelector(".languageChoose");
-const slides = document.querySelectorAll('.slide');
-const dots = document.querySelectorAll('.dot');
-const nextBtn = document.querySelector('.move.next');
-const prevBtn = document.querySelector('.move.prev');
 function getDeviceType() {
     const ua = navigator.userAgent.toLowerCase();
     const isTablet = /(ipad|tablet|(android(?!.*mobile)))/i.test(ua);
@@ -102,71 +98,3 @@ if (window.location.pathname.split("/")[1].toUpperCase() == "BG") {
     langBtn.childNodes[0].textContent = "EN";
     document.documentElement.setAttribute("lang", "en");
 }
-
-let current = 0;
-let interval;
-const delay = 4000;
-
-function showSlide(index) {
-    slides.forEach(slide => slide.classList.remove('current'));
-    dots.forEach(dot => dot.classList.remove('current'));
-
-    slides[index].classList.add('current');
-    dots[index].classList.add('current');
-    current = index;
-}
-
-function nextSlide() {
-    current = (current + 1) % slides.length;
-    showSlide(current);
-}
-
-function prevSlide() {
-    current = (current - 1 + slides.length) % slides.length;
-    showSlide(current);
-}
-
-function startAuto() {
-    interval = setInterval(nextSlide, delay);
-}
-
-function stopAuto() {
-    clearInterval(interval);
-}
-
-nextBtn.addEventListener('click', () => {
-    nextSlide();
-    stopAuto();
-    startAuto();
-});
-
-prevBtn.addEventListener('click', () => {
-    prevSlide();
-    stopAuto();
-    startAuto();
-});
-
-dots.forEach(dot => {
-    dot.addEventListener('click', e => {
-        showSlide(+e.target.dataset.index);
-        stopAuto();
-        startAuto();
-    });
-});
-
-const slider = document.querySelector('.slideshow_container');
-slider.addEventListener('mouseenter', stopAuto);
-slider.addEventListener('mouseleave', startAuto);
-
-let startX = 0;
-
-slider.addEventListener('touchstart', e => {
-    startX = e.touches[0].clientX;
-});
-
-slider.addEventListener('touchend', e => {
-    let endX = e.changedTouches[0].clientX;
-    if (startX - endX > 50) nextSlide();
-    if (endX - startX > 50) prevSlide();
-});
-startAuto();
